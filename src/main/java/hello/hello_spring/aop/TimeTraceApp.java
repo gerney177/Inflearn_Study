17547751b2e7663f76bef1446758c3f4b9b2b492
gerney177 <taegun0122@gmail.com>
2025-09-21 13:41:29 +0900

    Learning AOP

---
+++ src/main/java/hello/hello_spring/aop/TimeTraceApp.java
@@ -0,0 +1,25 @@
+package hello.hello_spring.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Aspect // aop 사용 시 필수
+@Component
+public class TimeTraceApp {
+
+    @Around("execution(* hello.hello_spring..*(..))") // 이 위치에 있는 애들 추적
+    public Object execution(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("START : " + joinPoint.toString());
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END : " + joinPoint.toString() + " " + timeMs + "ms");
+        }
+    }
+}
